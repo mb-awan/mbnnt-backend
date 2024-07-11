23821f071368a8785d0904ec1b562c51c7e47277
mb-awan <bilalofficialmail123@gmail.com>
2024-07-11 05:07:24 +0500

    feat: update role as admin assign permission to role as admin

--- src/common/controllers/permission/index.ts
+++ src/common/controllers/permission/index.ts
@@ -1,7 +1,10 @@
 import { Request, Response } from 'express';
 import { StatusCodes } from 'http-status-codes';
+import mongoose from 'mongoose';
 
+import { AdminPermissions, StudentPermissions, TeacherPermissions, VisitorPermissions } from '@/common/constants/enums';
 import { Permission } from '@/common/models/permissions';
+import { Role } from '@/common/models/roles';
 
 export const createPermission = async (req: Request, res: Response) => {
   try {
@@ -69,41 +72,97 @@ export const getSinglePermission = async (req: Request, res: Response) => {
 };
 
 export const updatePermission = async (req: Request, res: Response) => {
+  const { id } = req.query;
+  const { name, description } = req.body;
+
   try {
-    const { id } = req.query;
-    if (!id) {
-      return res.status(StatusCodes.BAD_REQUEST).json({ error: 'Permission ID is required' });
+    if (!id || !mongoose.Types.ObjectId.isValid(id.toString())) {
+      return res.status(StatusCodes.BAD_REQUEST).json({ message: 'Invalid permission ID.' });
     }
 
-    if (!req.body || Object.keys(req.body).length === 0) {
-      return res.status(StatusCodes.BAD_REQUEST).json({ error: 'Update data is required' });
-    }
-
-    const permission = await Permission.findByIdAndUpdate(id, req.body, { new: true, runValidators: true });
+    const permission = await Permission.findById(id);
     if (!permission) {
-      return res.status(StatusCodes.NOT_FOUND).json({ error: 'Permission not found' });
+      return res.status(StatusCodes.NOT_FOUND).json({ message: 'Permission not found.' });
     }
-    res.status(StatusCodes.OK).json({ message: 'Successfully updated permission', permission });
+
+    const seedPermissions = [
+      ...Object.values(VisitorPermissions),
+      ...Object.values(StudentPermissions),
+      ...Object.values(TeacherPermissions),
+      ...Object.values(AdminPermissions),
+    ];
+
+    if (
+      seedPermissions.includes(
+        permission.name as VisitorPermissions | StudentPermissions | TeacherPermissions | AdminPermissions
+      )
+    ) {
+      return res
+        .status(StatusCodes.BAD_REQUEST)
+        .json({ message: `The permission '${permission.name}' cannot be modified because it is seeded.` });
+    }
+
+    if (!name || typeof name !== 'string') {
+      return res.status(StatusCodes.BAD_REQUEST).json({ message: 'Name is required and must be a string.' });
+    }
+    if (name !== permission.name) {
+      const existingPermission = await Permission.findOne({ name });
+      if (existingPermission) {
+        return res.status(StatusCodes.BAD_REQUEST).json({ message: `Permission name '${name}' already exists.` });
+      }
+    }
+    permission.name = name;
+    permission.description = description;
+    await permission.save();
+
+    res.status(StatusCodes.OK).json({ message: `Permission '${permission.name}' updated successfully.` });
   } catch (error) {
-    console.error('Error updating permission:', error);
-    res.status(StatusCodes.BAD_REQUEST).json({ error: 'Failed to update permission' });
+    res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ message: 'Internal server error', error });
   }
 };
 
 export const deletePermission = async (req: Request, res: Response) => {
   try {
-    const { id } = req.query;
-    if (!id) {
-      return res.status(StatusCodes.BAD_REQUEST).json({ error: 'Permission ID is required' });
+    const { id, name } = req.query;
+    if (!id && !name) {
+      return res.status(StatusCodes.BAD_REQUEST).json({ error: 'Permission ID or name is required' });
     }
-
-    const permission = await Permission.findByIdAndDelete(id);
+    let permission;
+    if (id) {
+      permission = await Permission.findById(id);
+    } else if (name) {
+      permission = await Permission.findOne({ name });
+    }
     if (!permission) {
-      return res.status(StatusCodes.NOT_FOUND).json({ error: 'Permission not found' });
+      return res.status(StatusCodes.NOT_FOUND).json({ message: `Permission not found.` });
     }
-    res.status(StatusCodes.OK).json({ message: 'Successfully deleted permission', permission });
+    const seedPermissions = [
+      ...Object.values(VisitorPermissions),
+      ...Object.values(StudentPermissions),
+      ...Object.values(TeacherPermissions),
+      ...Object.values(AdminPermissions),
+    ];
+
+    if (
+      seedPermissions.includes(
+        permission.name as VisitorPermissions | StudentPermissions | TeacherPermissions | AdminPermissions
+      )
+    ) {
+      return res
+        .status(StatusCodes.BAD_REQUEST)
+        .json({ message: `The permission '${permission.name}' cannot be modified because it is seeded.` });
+    }
+    const roleCount = await Role.countDocuments({ permissions: permission._id });
+
+    if (roleCount > 1) {
+      return res.status(StatusCodes.BAD_REQUEST).json({
+        message: `Cannot delete the permission '${permission.name}' because it is assigned to more than one roles.`,
+      });
+    }
+    await permission.deleteOne();
+
+    res.status(StatusCodes.OK).json({ message: `Permission '${permission.name}' deleted successfully.` });
   } catch (error) {
-    console.error('Error deleting permission:', error);
-    res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ error: 'Failed to delete permission' });
+    res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ message: 'Internal server error', error });
   }
 };
--- src/common/controllers/role/index.ts
+++ src/common/controllers/role/index.ts
@@ -1,7 +1,11 @@
 import { Request, Response } from 'express';
 import { StatusCodes } from 'http-status-codes';
+import mongoose, { isValidObjectId } from 'mongoose';
 
+import { UserRoles } from '@/common/constants/enums';
+import { Permission } from '@/common/models/permissions';
 import { Role } from '@/common/models/roles';
+import { User } from '@/common/models/user';
 
 export const createRole = async (req: Request, res: Response) => {
   try {
@@ -72,35 +76,77 @@ export const getSingleRole = async (req: Request, res: Response) => {
 };
 
 export const updateRole = async (req: Request, res: Response) => {
+  const { id } = req.query;
+  const { name } = req.body;
+
   try {
-    const { id } = req.query;
-    if (!id) {
-      return res.status(StatusCodes.BAD_REQUEST).json({ error: 'Role ID is required' });
+    if (!id || !mongoose.Types.ObjectId.isValid(id.toString())) {
+      return res.status(StatusCodes.BAD_REQUEST).json({ message: 'Invalid Role ID.' });
     }
 
-    if (!req.body || Object.keys(req.body).length === 0) {
-      return res.status(StatusCodes.BAD_REQUEST).json({ error: 'Update data is required' });
-    }
-
-    const role = await Role.findByIdAndUpdate(id, req.body, { new: true, runValidators: true }).populate('permissions');
+    const role = await Role.findById(id);
     if (!role) {
-      return res.status(StatusCodes.NOT_FOUND).json({ error: 'Role not found' });
+      return res.status(StatusCodes.NOT_FOUND).json({ message: 'Role not found.' });
     }
-    res.status(StatusCodes.OK).json({ message: 'Successfully updated role', role });
+
+    const protectedRoles = [
+      UserRoles.ADMIN,
+      UserRoles.SUB_ADMIN,
+      UserRoles.TEACHER,
+      UserRoles.STUDENT,
+      UserRoles.VISITOR,
+    ];
+    if (protectedRoles.includes(role?.name as UserRoles)) {
+      return res.status(StatusCodes.BAD_REQUEST).json({ message: `The role '${role?.name}' cannot be edited.` });
+    }
+
+    if (!name || typeof name !== 'string') {
+      return res.status(StatusCodes.BAD_REQUEST).json({ message: 'Name is required and must be a string.' });
+    }
+    if (name !== role.name) {
+      const existingPermission = await Role.findOne({ name });
+      if (existingPermission) {
+        return res.status(StatusCodes.BAD_REQUEST).json({ message: `Role name '${name}' already exists.` });
+      }
+    }
+    role.name = name;
+    await role.save();
+
+    res.status(StatusCodes.OK).json({ message: `Role '${role.name}' updated successfully.` });
   } catch (error) {
-    console.error('Error updating role:', error);
-    res.status(StatusCodes.BAD_REQUEST).json({ error: 'Failed to update role' });
+    res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ message: 'Internal server error', error });
   }
 };
 
 export const deleteRole = async (req: Request, res: Response) => {
   try {
-    const { id } = req.query;
-    if (!id) {
-      return res.status(StatusCodes.BAD_REQUEST).json({ error: 'Role ID is required' });
+    const { id, name } = req.query;
+    if (!id && !name) {
+      return res.status(StatusCodes.BAD_REQUEST).json({ error: 'Role ID or name is required' });
     }
+    let role;
+    if (id) {
+      role = await Role.findById(id);
+    } else if (name) {
+      role = await Role.findOne({ name });
+    }
+    const protectedRoles = [
+      UserRoles.ADMIN,
+      UserRoles.SUB_ADMIN,
+      UserRoles.TEACHER,
+      UserRoles.STUDENT,
+      UserRoles.VISITOR,
+    ];
+    if (protectedRoles.includes(role?.name as UserRoles)) {
+      return res.status(StatusCodes.BAD_REQUEST).json({ message: `The role '${role?.name}' cannot be deleted.` });
+    }
+    const userCount = await Role.countDocuments({ role: role?.name });
 
-    const role = await Role.findByIdAndDelete(id);
+    if (userCount > 0) {
+      return res
+        .status(StatusCodes.BAD_REQUEST)
+        .json({ message: `Cannot delete the role '${role?.name}' because it is assigned to one or more users.` });
+    }
     if (!role) {
       return res.status(StatusCodes.NOT_FOUND).json({ error: 'Role not found' });
     }
@@ -108,5 +154,78 @@ export const deleteRole = async (req: Request, res: Response) => {
   } catch (error) {
     console.error('Error deleting role:', error);
     res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ error: 'Failed to delete role' });
+  }
+};
+
+export const AssignPermissiontoRole = async (req: Request, res: Response) => {
+  try {
+    const { id } = req.query;
+    const { name, description } = req.body;
+
+    const existingPermission = await Permission.findOne({ name });
+
+    if (existingPermission) {
+      return res.status(StatusCodes.BAD_REQUEST).json({ message: `Permission '${name}' already exists.` });
+    }
+    const permission = new Permission({ name, description });
+    await permission.save();
+
+    const role = await Role.findById(id);
+    if (!role) {
+      return res.status(StatusCodes.NOT_FOUND).json({ error: 'Role not found' });
+    }
+    if (!role.permissions.includes(permission._id as mongoose.Types.ObjectId)) {
+      role.permissions.push(permission._id as mongoose.Types.ObjectId);
+      await role.save();
+    }
+    res.status(StatusCodes.OK).json({ message: 'Permission assigned to role successfully' });
+  } catch (error) {
+    console.error('Error assigning permission to role:', error);
+    res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ error: 'Failed to assign permission to role' });
+  }
+};
+
+export const updateUserRole = async (req: Request, res: Response) => {
+  try {
+    const { id, email, phone, username, roleId } = req.query;
+
+    if (!email && !phone && !username) {
+      return res
+        .status(StatusCodes.BAD_REQUEST)
+        .json({ error: 'At least one identifier (email, phone, id, or username) must be provided.' });
+    }
+
+    const user = email
+      ? await User.findOne({ email })
+      : phone
+        ? await User.findOne({ phone })
+        : id
+          ? await User.findById(id)
+          : username
+            ? await User.findOne({ username })
+            : null;
+
+    if (!user) {
+      return res.status(StatusCodes.NOT_FOUND).json({ error: 'User not found.' });
+    }
+
+    if (!roleId) {
+      return res.status(StatusCodes.BAD_REQUEST).json({ error: 'Role ID must be provided.' });
+    }
+
+    if (!isValidObjectId(roleId)) {
+      return res.status(StatusCodes.BAD_REQUEST).json({ error: 'Invalid Role ID format.' });
+    }
+
+    const existingRole = await Role.findById(roleId);
+    if (!existingRole) {
+      return res.status(StatusCodes.NOT_FOUND).json({ error: 'Role not found.' });
+    }
+    user.role = existingRole._id as mongoose.Types.ObjectId;
+    await user.save();
+    return res.status(StatusCodes.OK).json({ message: 'User role updated successfully.' });
+  } catch (error) {
+    console.error('Error updating user role:', error);
+    return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ error: 'Internal Server Error.' });
   }
 };
